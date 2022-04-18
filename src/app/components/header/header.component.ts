9fcba4f8c0e97e7d70f78888b1ec9590678f687b
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-04-19 01:42:37 +0200

    Update header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../../interfaces/category.interface';
+
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+	categories: Category[];
+	selectedCategory: Category;
 	isCartVisible: boolean = false;
 
-	constructor() { }
+	constructor() {
+		this.categories = [
+			{
+				id: 0,
+				name: 'All categories',
+				slug: ''
+			},
+			{
+				id: 1,
+				name: 'Category 1',
+				slug: 'category-1'
+			},
+			{
+				id: 2,
+				name: 'Category 2',
+				slug: 'category-2'
+			},
+			{
+				id: 3,
+				name: 'Category 3',
+				slug: 'category-4'
+			}
+		];
+
+		this.selectedCategory = this.categories[0];
+	}
 
 	ngOnInit(): void {
 	}
 
-	onCartClose() {
+	onCategorySelected(category: Category) {
+		this.selectedCategory = category;
+	}
+
+	onCartClosed() {
 		this.isCartVisible = false;
 	}
 }
