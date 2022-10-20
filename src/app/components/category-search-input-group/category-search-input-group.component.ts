0fd629d84ab8e0f44fe888ff1e64488508448801
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-20 21:26:13 +0200

    Add translation for category search input group
    - Add Cairo and NotoSansArabic fonts.
    - Add translation and rtl style.

--- src/app/components/category-search-input-group/category-search-input-group.component.ts
+++ src/app/components/category-search-input-group/category-search-input-group.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 import { Category } from '../../interfaces/category.interface';
 
@@ -7,18 +9,28 @@ import { Category } from '../../interfaces/category.interface';
 	templateUrl: './category-search-input-group.component.html',
 	styleUrls: ['./category-search-input-group.component.scss']
 })
-export class CategorySearchInputGroupComponent implements OnInit {
+export class CategorySearchInputGroupComponent implements OnInit, OnDestroy {
 	@Input() categories: Category[] = [];
 	@Output() onSearch: EventEmitter<{ search: string, category: Category }> = new EventEmitter<{ search: string, category: Category }>();
 	@ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+	translateSubscription!: Subscription;
+	rtl: boolean = false;
 	selectedCategory!: Category;
 
-	constructor() { }
+	constructor(private translateService: TranslateService) { }
 
 	ngOnInit(): void {
+		this.translateSubscription = this.translateService.onLangChange.subscribe(event => {
+			this.rtl = event.translations.direction === 'rtl';
+		});
+
 		if (this.categories.length > 1) {
 			this.selectedCategory = this.categories[0];
 		}
+	}
+
+	ngOnDestroy(): void {
+		this.translateSubscription.unsubscribe();
 	}
 
 	onCategorySelected(category: Category) {
@@ -28,15 +40,15 @@ export class CategorySearchInputGroupComponent implements OnInit {
 	onSearchKeyPressed(event: KeyboardEvent) {
 		if (event.key === 'Enter') {
 			event.preventDefault();
-
-			this.onSearch.emit({
-				search: this.searchInput.nativeElement.value,
-				category: this.selectedCategory
-			});
+			this.emitSearchEvent();
 		}
 	}
 
 	onSearchIconClicked(event: MouseEvent) {
+		this.emitSearchEvent();
+	}
+
+	emitSearchEvent() {
 		this.onSearch.emit({
 			search: this.searchInput.nativeElement.value,
 			category: this.selectedCategory
