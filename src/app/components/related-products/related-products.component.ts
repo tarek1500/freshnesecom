16dbc407c6139a10cbb42bc6198d491c39ee6bfb
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-25 23:27:40 +0200

    Add translation for related products
    - Add translation and rtl style.

--- src/app/components/related-products/related-products.component.ts
+++ src/app/components/related-products/related-products.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../services/rtl/rtl.service';
 import { Product } from '../../interfaces/product.interface';
 
 @Component({
@@ -7,12 +9,23 @@ import { Product } from '../../interfaces/product.interface';
 	templateUrl: './related-products.component.html',
 	styleUrls: ['./related-products.component.scss']
 })
-export class RelatedProductsComponent implements OnInit {
+export class RelatedProductsComponent implements OnInit, OnDestroy {
 	@Input() heading: string = '';
 	@Input('goto-button') gotoButton: string = '';
 	@Input() products!: Product[];
+	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
 
-	constructor() { }
+	constructor(private rtlService: RtlService) { }
 
-	ngOnInit(): void { }
+	ngOnInit(): void {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+		});
+		this.subscriptions.push(subscription);
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
+	}
 }
