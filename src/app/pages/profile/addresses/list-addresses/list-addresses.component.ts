99f01fa80665ce7083741671620c1c0b10355b53
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-27 17:02:18 +0200

    Add translation for profile addresses page
    - Add translation and rtl style.

--- src/app/pages/profile/addresses/list-addresses/list-addresses.component.ts
+++ src/app/pages/profile/addresses/list-addresses/list-addresses.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../../../services/rtl/rtl.service';
 import { Address } from '../../../../interfaces/address.interface';
 
 @Component({
@@ -8,13 +10,16 @@ import { Address } from '../../../../interfaces/address.interface';
 	templateUrl: './list-addresses.component.html',
 	styleUrls: ['./list-addresses.component.scss']
 })
-export class ListAddressesComponent implements OnInit {
-	addresses!: Address[];
+export class ListAddressesComponent implements OnInit, OnDestroy {
+	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
+	addresses: Address[];
 	modalTitle!: string;
 
-	constructor(private modalService: NgbModal) { }
-
-	ngOnInit(): void {
+	constructor(
+		private rtlService: RtlService,
+		private modalService: NgbModal
+	) {
 		this.addresses = [
 			{
 				id: 1,
@@ -39,9 +44,69 @@ export class ListAddressesComponent implements OnInit {
 		];
 	}
 
+	ngOnInit(): void {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+
+			if (this.rtl) {
+				this.addresses = [
+					{
+						id: 1,
+						title: 'العنوان الرئيسي',
+						address: '1234 شارع فريشنس كوم',
+						city: 'سان فرانسيسكو، كاليفورنيا',
+						country: 'الولايات المتحدة الأمريكية',
+						postal: '12345',
+						latitude: 31.259672,
+						longitude: 29.996615
+					},
+					{
+						id: 2,
+						title: 'العنوان الثانوي',
+						address: '1234 شارع فريشنس كوم',
+						city: 'سان فرانسيسكو، كاليفورنيا',
+						country: 'الولايات المتحدة الأمريكية',
+						postal: '12345',
+						latitude: 31.228255,
+						longitude: 29.952374
+					}
+				];
+			}
+			else {
+				this.addresses = [
+					{
+						id: 1,
+						title: 'Main address',
+						address: '1234 Freshnesecom St',
+						city: 'San Francisco, CA',
+						country: 'USA',
+						postal: '12345',
+						latitude: 31.259672,
+						longitude: 29.996615
+					},
+					{
+						id: 2,
+						title: 'Secondary address',
+						address: '1234 Freshnesecom St',
+						city: 'San Francisco, CA',
+						country: 'USA',
+						postal: '12345',
+						latitude: 31.228255,
+						longitude: 29.952374
+					}
+				];
+			}
+		});
+		this.subscriptions.push(subscription);
+	}
+
 	openDeleteModal(content: TemplateRef<any>, address: Address) {
 		this.modalTitle = address.title;
 
-		this.modalService.open(content, { size: 'lg', centered: true });
+		this.modalService.open(content, { centered: true, size: 'lg', windowClass: this.rtl ? 'rtl' : '' });
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
 	}
 }
