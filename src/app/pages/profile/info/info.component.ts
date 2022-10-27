f56c2bbb48e42684fad03525ba623aa13d8607ee
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-27 16:37:46 +0200

    Add translation for profile info page
    - Add translation and rtl style.

--- src/app/pages/profile/info/info.component.ts
+++ src/app/pages/profile/info/info.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../../services/rtl/rtl.service';
 import { User } from '../../../interfaces/user.interface';
 
 @Component({
@@ -7,13 +9,12 @@ import { User } from '../../../interfaces/user.interface';
 	templateUrl: './info.component.html',
 	styleUrls: ['./info.component.scss']
 })
-export class InfoComponent implements OnInit {
-	user!: User;
+export class InfoComponent implements OnInit, OnDestroy {
+	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
+	user: User;
 
-	constructor() { }
-
-	ngOnInit(): void {
-		// Fetch user from server
+	constructor(private rtlService: RtlService) {
 		this.user = {
 			id: 1,
 			name: 'Author',
@@ -22,5 +23,37 @@ export class InfoComponent implements OnInit {
 			role: '',
 			image: 'https://picsum.photos/id/237/150/150'
 		};
+	}
+
+	ngOnInit(): void {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+
+			if (this.rtl) {
+				this.user = {
+					id: 1,
+					name: 'المؤلف',
+					email: 'author@domain.com',
+					phone: '+420336775664',
+					role: '',
+					image: 'https://picsum.photos/id/237/150/150'
+				};
+			}
+			else {
+				this.user = {
+					id: 1,
+					name: 'Author',
+					email: 'author@domain.com',
+					phone: '+420336775664',
+					role: '',
+					image: 'https://picsum.photos/id/237/150/150'
+				};
+			}
+		});
+		this.subscriptions.push(subscription);
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
 	}
 }
--- src/app/pages/profile/info/info.module.ts
+++ src/app/pages/profile/info/info.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 
 import { InfoRoutingModule } from './info-routing.module';
@@ -11,6 +12,7 @@ import { InfoComponent } from './info.component';
 	],
 	imports: [
 		CommonModule,
+		TranslateModule,
 		FormsModule,
 		InfoRoutingModule
 	]
