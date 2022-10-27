352cbb47ac9b143a7998382454d9e3a6e5a1de05
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-27 16:36:16 +0200

    Add translation for profile page
    - Add translation and rtl style.

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
 
+import { RtlService } from '../../services/rtl/rtl.service';
 import { Breadcrumb } from '../../interfaces/breadcrumb.interface';
 
 @Component({
@@ -11,17 +12,26 @@ import { Breadcrumb } from '../../interfaces/breadcrumb.interface';
 })
 export class ProfileComponent implements OnInit, OnDestroy {
 	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
 	breadcrumb: Breadcrumb[] = [
-		{ translate: '', text: 'Home', link: '/' },
-		{ translate: '', text: 'Profile', link: '' }
+		{ translate: 'translate.components.breadcrumb.home', text: 'Home', link: '/' },
+		{ translate: 'translate.components.breadcrumb.profile', text: 'Profile', link: '' }
 	];
 	url!: string;
 	active: string = 'info';
 
-	constructor(private router: Router) { }
+	constructor(
+		private rtlService: RtlService,
+		private router: Router
+	) { }
 
 	ngOnInit(): void {
-		let subscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+		});
+		this.subscriptions.push(subscription);
+
+		subscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
 			this.url = (event as NavigationEnd).url;
 		});
 		this.subscriptions.push(subscription);
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProfileRoutingModule } from './profile-routing.module';
@@ -13,6 +14,7 @@ import { ProfileComponent } from './profile.component';
 	],
 	imports: [
 		CommonModule,
+		TranslateModule,
 		ProfileRoutingModule,
 		NgbModule,
 		SharedModule,
