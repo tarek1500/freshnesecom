89c0f93be836ab8430f5f8da0ae734076e8b24cb
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-10-30 23:01:02 +0200

    Add translation for related blogs
    - Add translation and rtl style.

--- src/app/components/blog-details/related-blogs/related-blogs.component.ts
+++ src/app/components/blog-details/related-blogs/related-blogs.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../../services/rtl/rtl.service';
 import { Blog } from '../../../interfaces/blog.interface';
 
 @Component({
@@ -7,10 +9,21 @@ import { Blog } from '../../../interfaces/blog.interface';
 	templateUrl: './related-blogs.component.html',
 	styleUrls: ['./related-blogs.component.scss']
 })
-export class RelatedBlogsComponent implements OnInit {
+export class RelatedBlogsComponent implements OnInit, OnDestroy {
 	@Input() blogs!: Blog[];
+	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
 
-	constructor() { }
+	constructor(private rtlService: RtlService) { }
 
-	ngOnInit(): void { }
+	ngOnInit(): void {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+		});
+		this.subscriptions.push(subscription);
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
+	}
 }
