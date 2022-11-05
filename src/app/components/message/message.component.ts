a6ace42bdfb1f578ef24f9d27ff6ac0e362a7d90
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-11-05 20:28:30 +0200

    Add translation for message
    - Add translation and rtl style.

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../services/rtl/rtl.service';
 import { Message } from '../../interfaces/message.interface';
 
 @Component({
@@ -8,12 +10,23 @@ import { Message } from '../../interfaces/message.interface';
 	templateUrl: './message.component.html',
 	styleUrls: ['./message.component.scss']
 })
-export class MessageComponent implements OnInit {
-	moment: any = moment;
+export class MessageComponent implements OnInit, OnDestroy {
 	@Input() message!: Message;
 	@Input('right-position') rightPosition: boolean = false;
+	moment: any = moment;
+	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
 
-	constructor() { }
+	constructor(private rtlService: RtlService) { }
 
-	ngOnInit(): void { }
+	ngOnInit(): void {
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+		});
+		this.subscriptions.push(subscription);
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
+	}
 }
