f9b59a2214450bbb80e34c35678bba9e10c5f4b4
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-11-05 21:47:27 +0200

    Create local storage service

---
+++ src/app/services/local-storage/local-storage.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LocalStorageService {
+	constructor() { }
+
+	setItem(key: string, value: any) {
+		localStorage.setItem(key, value);
+	}
+
+	getItem(key: string) {
+		return localStorage.getItem(key);
+	}
+
+	removeItem(key: string) {
+		localStorage.removeItem(key);
+	}
+
+	clear() {
+		localStorage.clear();
+	}
+}
