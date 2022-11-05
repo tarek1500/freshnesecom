7f6471d6e91578cb59f6c5ac4f496790c685481b
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-11-05 20:29:14 +0200

    Add translation for chat
    - Add translation and rtl style.

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
+import { RtlService } from '../../services/rtl/rtl.service';
 import { ChatService } from '../../services/chat/chat.service';
 import { Message } from '../../interfaces/message.interface';
 
@@ -11,89 +12,173 @@ import { Message } from '../../interfaces/message.interface';
 })
 export class ChatComponent implements OnInit, OnDestroy {
 	subscriptions: Subscription[] = [];
+	rtl: boolean = false;
 	isChatVisible: boolean = false;
-	isTyping: boolean = false;
+	isTyping: boolean = true;
 	messages!: Message[];
 
-	constructor(private chatService: ChatService) { }
+	constructor(
+		private rtlService: RtlService,
+		private chatService: ChatService
+	) { }
 
 	ngOnInit(): void {
-		let subscription = this.chatService.chatSubject$.subscribe(chat => {
-			this.isChatVisible = chat;
+		let subscription = this.rtlService.rtlSubject$.subscribe(rtl => {
+			this.rtl = rtl;
+
+			this.loadMessages();
 		});
 		this.subscriptions.push(subscription);
 
-		this.messages = [
-			{
-				id: 1,
-				author: {
-					id: 1,
-					name: 'Admin',
-					email: '',
-					phone: '',
-					role: '',
-					image: 'https://picsum.photos/id/237/40/40'
-				},
-				date: new Date('2022-4-28'),
-				content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-			},
-			{
-				id: 1,
-				author: {
-					id: 2,
-					name: 'User',
-					email: '',
-					phone: '',
-					role: '',
-					image: 'https://picsum.photos/id/237/40/40'
-				},
-				date: new Date('2022-4-29'),
-				content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-			},
-			{
-				id: 3,
-				author: {
-					id: 1,
-					name: 'Admin',
-					email: '',
-					phone: '',
-					role: '',
-					image: 'https://picsum.photos/id/237/40/40'
-				},
-				date: new Date('2022-4-30'),
-				content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-			},
-			{
-				id: 4,
-				author: {
-					id: 2,
-					name: 'User',
-					email: '',
-					phone: '',
-					role: '',
-					image: 'https://picsum.photos/id/237/40/40'
-				},
-				date: new Date('2022-5-1'),
-				content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-			},
-			{
-				id: 5,
-				author: {
-					id: 1,
-					name: 'Admin',
-					email: '',
-					phone: '',
-					role: '',
-					image: 'https://picsum.photos/id/237/40/40'
-				},
-				date: new Date('2022-5-2'),
-				content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-			}
-		];
+		subscription = this.chatService.chatSubject$.subscribe(chat => {
+			this.isChatVisible = chat;
+		});
+		this.subscriptions.push(subscription);
 	}
 
 	ngOnDestroy(): void {
 		this.subscriptions.forEach(subscription => subscription.unsubscribe());
+	}
+
+	loadMessages() {
+		if (this.rtl) {
+			this.messages = [
+				{
+					id: 1,
+					author: {
+						id: 1,
+						name: 'المشرف',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-28'),
+					content: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت.'
+				},
+				{
+					id: 2,
+					author: {
+						id: 2,
+						name: 'المستخدم',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-29'),
+					content: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت.'
+				},
+				{
+					id: 3,
+					author: {
+						id: 1,
+						name: 'المشرف',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-30'),
+					content: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت.'
+				},
+				{
+					id: 4,
+					author: {
+						id: 2,
+						name: 'المستخدم',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-5-1'),
+					content: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت.'
+				},
+				{
+					id: 5,
+					author: {
+						id: 1,
+						name: 'المشرف',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-5-2'),
+					content: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت.'
+				}
+			];
+		}
+		else {
+			this.messages = [
+				{
+					id: 1,
+					author: {
+						id: 1,
+						name: 'Admin',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-28'),
+					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+				},
+				{
+					id: 2,
+					author: {
+						id: 2,
+						name: 'User',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-29'),
+					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+				},
+				{
+					id: 3,
+					author: {
+						id: 1,
+						name: 'Admin',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-4-30'),
+					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+				},
+				{
+					id: 4,
+					author: {
+						id: 2,
+						name: 'User',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-5-1'),
+					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+				},
+				{
+					id: 5,
+					author: {
+						id: 1,
+						name: 'Admin',
+						email: '',
+						phone: '',
+						role: '',
+						image: 'https://picsum.photos/id/237/40/40'
+					},
+					date: new Date('2022-5-2'),
+					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+				}
+			];
+		}
 	}
 
 	onSent(event: string) { }
