d83861001c6a0043938ec7a378d1a5fb4c9a1268
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-11-06 12:49:18 +0200

    Add translation for privacy policy page
    - Add translation and rtl style.

--- src/app/pages/privacy-policy/privacy-policy.module.ts
+++ src/app/pages/privacy-policy/privacy-policy.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { PrivacyPolicyRoutingModule } from './privacy-policy-routing.module';
 import { SharedModule } from '../../modules/shared/shared.module';
@@ -12,6 +13,7 @@ import { PrivacyPolicyComponent } from './privacy-policy.component';
 	],
 	imports: [
 		CommonModule,
+		TranslateModule,
 		PrivacyPolicyRoutingModule,
 		SharedModule,
 		BreadcrumbModule
